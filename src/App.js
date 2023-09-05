e5a97c545800e9b19fc360a0406312583ef447b3
emily-guan <emily.guan27@hotmail.com>
2023-09-05 16:34:57 +1000

    Install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+        crossorigin="anonymous"
+      />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,6 +24,9 @@ function App() {
         >
           Learn React
         </a>
+        <Button as="a" variant="primary">
+          Button as link
+        </Button>
       </header>
     </div>
   );
